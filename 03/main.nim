fc5d4a044722bdfd9d691232b6129f78cfd94142
Ico Doornekamp <i.doornekamp@activevideo.com>
2020-12-03 15:49:48 +0100

    runall

--- 03/main.nim
+++ 03/main.nim
@@ -1,16 +1,16 @@
 import sequtils, math
 
-let
-  input = readfile "input"
-  h = input.count('\n')
-  w = input.find('\n')
-  slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+const
+  m = slurp "input"
+  h = m.count('\n')
+  w = m.find('\n')
+  ss = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
 proc isTree(s: tuple[x, y: int]): bool =
-  input[s.y * (w+1) + (s.x mod w)] == '#'
+  m[s.y * (w+1) + (s.x mod w)] == '#'
 
 proc run(s: tuple[x, y: int]): int =
   toSeq(0 ..< h/%s.y).countIt(isTree (it*s.x, it*s.y))
 
 echo "part1: ", run (3, 1)
-echo "part2: ", slopes.map(run).prod
+echo "part2: ", ss.map(run).prod
